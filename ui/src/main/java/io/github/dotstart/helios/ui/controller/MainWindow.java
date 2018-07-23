94bab275c7dd978a756d71a4f56792628dd8ba32
Johannes Donath <johannesd@torchmind.com>
2018-07-23 02:19:19 +0200

    More debug logging and improved field naming.

--- ui/src/main/java/io/github/dotstart/helios/ui/controller/MainWindow.java
+++ ui/src/main/java/io/github/dotstart/helios/ui/controller/MainWindow.java
@@ -16,9 +16,11 @@
  */
 package io.github.dotstart.helios.ui.controller;
 
+import com.google.inject.Inject;
 import edu.umd.cs.findbugs.annotations.NonNull;
 import io.github.dotstart.helios.api.layout.TimerLayout;
 import io.github.dotstart.helios.api.node.layout.SwitchLayout;
+import io.github.dotstart.helios.ui.module.component.TimerComponent;
 import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.beans.binding.Bindings;
@@ -26,6 +28,8 @@ import javafx.beans.property.ObjectProperty;
 import javafx.beans.property.SimpleObjectProperty;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 /**
  * Provides a controller for the main timer window.
@@ -34,10 +38,18 @@ import javafx.fxml.Initializable;
  */
 public class MainWindow implements Initializable {
 
+  private static final Logger logger = LogManager.getFormatterLogger(MainWindow.class);
+
+  private final TimerComponent defaultComponent;
   private final ObjectProperty<TimerLayout> layout = new SimpleObjectProperty<>();
 
   @FXML
-  private SwitchLayout components;
+  private SwitchLayout componentPane;
+
+  @Inject
+  public MainWindow(@NonNull TimerComponent defaultComponent) {
+    this.defaultComponent = defaultComponent;
+  }
 
   /**
    * {@inheritDoc}
@@ -46,25 +58,55 @@ public class MainWindow implements Initializable {
   public void initialize(URL location, ResourceBundle resources) {
     this.layout.addListener((observable, oldValue, newValue) -> {
       if (oldValue != null) {
-        Bindings.unbindContent(this.components.getChildren(), newValue.getNodes());
+        logger.debug("Removing bindings from previous timer layout");
+        Bindings.unbindContent(this.componentPane.getNodes(), newValue.getNodes());
       }
 
       if (newValue == null) {
-        this.layout.set(new TimerLayout());
+        logger.debug("Layout deleted - Constructing default layout");
+        this.layout.set(this.createDefaultLayout());
         return;
       }
 
-      Bindings.bindContent(this.components.getChildren(), newValue.getNodes());
+      logger.debug("Binding new timer layout (currently consisting of %d nodes)",
+          newValue.getNodes().size());
+      Bindings.bindContent(this.componentPane.getNodes(), newValue.getNodes());
     });
 
-    this.layout.set(new TimerLayout());
+    // TODO: Load previously loaded layout
+    this.layout.set(this.createDefaultLayout());
   }
 
+  /**
+   * <p>Creates a standardized timer layout.</p>
+   *
+   * <p>The standard layout is used in place of a user configuration when none is available or is
+   * unloaded via the context menu.</p>
+   *
+   * @return a standard timer layout.
+   */
+  @NonNull
+  private TimerLayout createDefaultLayout() {
+    var layout = new TimerLayout();
+    layout.addComponent(this.defaultComponent);
+    return layout;
+  }
+
+  /**
+   * Retrieves the currently loaded timer layout.
+   *
+   * @return a layout.
+   */
   @NonNull
   public TimerLayout getLayout() {
     return this.layout.get();
   }
 
+  /**
+   * Replaces the currently loaded timer layout.
+   *
+   * @param layout a new layout.
+   */
   public void setLayout(@NonNull TimerLayout layout) {
     this.layout.set(layout);
   }
